f55356ee743d4961ea3933c3af6541440204d003
Vedaad Shakib <vedaad.shakib@berkeley.edu>
2021-10-01 16:51:47 -0700

    Update interface to match AlphaZero

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 from minimax import minimax_select_move, is_winner, is_board_full
 
 def draw_board(board):
-        board = ["O" if i == 1 else "X" if i == -1 else " " for i in board]
         # This function prints out the board that it was passed.
         # "board" is a list of 10 strings representing the board (ignore index 0)
-        print(board[0] + '|' + board[1] + '|' + board[2])
+        print("|".join(["O" if i == 1 else "X" if i == -1 else " " for i in board[0]]))
         print('-+-+-')
-        print(board[3] + '|' + board[4] + '|' + board[5])
+        print("|".join(["O" if i == 1 else "X" if i == -1 else " " for i in board[1]]))
         print('-+-+-')
-        print(board[6] + '|' + board[7] + '|' + board[8])
+        print("|".join(["O" if i == 1 else "X" if i == -1 else " " for i in board[2]]))
 
 def get_player_move(board):
     # Let the player type in their move.
     move = '' 
-    while move not in range(9) or board[int(move)] != 0:
+    while move not in range(9) or board[int(move)//3][int(move)%3] != 0:
         print('What is your next move? (0-8)')
         move = int(input())
     return move
 
 if __name__ == "__main__":
     # Reset the board
-    theBoard = [0] * 9
+    theBoard = [[0 for _ in range(3)] for _ in range(3)]
     playerLetter, computerLetter = -1, 1
     # Player always goes first
     turn = "player"
@@ -31,7 +30,7 @@ if __name__ == "__main__":
         if turn == 'player':
             draw_board(theBoard)
             move = get_player_move(theBoard)
-            theBoard[move] = playerLetter
+            theBoard[move//3][move%3] = playerLetter
 
             if is_winner(theBoard, playerLetter):
                 draw_board(theBoard)
@@ -45,8 +44,8 @@ if __name__ == "__main__":
                 else:
                     turn = 'computer'
         else:
-            move = minimax_select_move(theBoard)
-            theBoard[move] = computerLetter
+            move, _ = minimax_select_move(theBoard)
+            theBoard[move//3][move%3] = computerLetter
 
             if is_winner(theBoard, computerLetter):
                 draw_board(theBoard)
--- minimax/minimax_agent.py
+++ minimax/minimax_agent.py
@@ -2,18 +2,18 @@ import random
 
 def is_winner(board, letter):
         # Given a board and a player's letter, this function returns True if that player has won.
-        return ((board[0]==letter and board[1]==letter and board[2]==letter) or
-                (board[3]==letter and board[4]==letter and board[5]==letter) or
-                (board[6]==letter and board[7]==letter and board[8]==letter) or
-                (board[0]==letter and board[3]==letter and board[6]==letter) or
-                (board[1]==letter and board[4]==letter and board[7]==letter) or
-                (board[2]==letter and board[5]==letter and board[8]==letter) or
-                (board[0]==letter and board[4]==letter and board[8]==letter) or
-                (board[2]==letter and board[4]==letter and board[6]==letter))
+        return ((board[0][0]==letter and board[0][1]==letter and board[0][2]==letter) or
+                (board[1][0]==letter and board[1][1]==letter and board[1][2]==letter) or
+                (board[2][0]==letter and board[2][1]==letter and board[2][2]==letter) or
+                (board[0][0]==letter and board[1][0]==letter and board[2][0]==letter) or
+                (board[0][1]==letter and board[1][1]==letter and board[2][1]==letter) or
+                (board[0][2]==letter and board[1][2]==letter and board[2][2]==letter) or
+                (board[0][0]==letter and board[1][1]==letter and board[2][2]==letter) or
+                (board[0][2]==letter and board[1][1]==letter and board[2][0]==letter))
 
 
 def is_space_free(board, move):
-        return board[move] == 0
+        return board[move//3][move%3] == 0
 
 def minimax(board, depth, isMax, alpha, beta):
         computerLetter, playerLetter = 1, -1
@@ -31,10 +31,10 @@ def minimax(board, depth, isMax, alpha, beta):
 
                 for i in range(9):
                         if is_space_free(board, i):
-                                board[i] = computerLetter
+                                board[i//3][i%3] = computerLetter
                                 best = max(best, minimax(board, depth+1, not isMax, alpha, beta) - depth)
                                 alpha = max(alpha, best)
-                                board[i] = 0
+                                board[i//3][i%3] = 0
 
                                 if alpha >= beta:
                                         break
@@ -45,10 +45,10 @@ def minimax(board, depth, isMax, alpha, beta):
 
                 for i in range(9):
                         if is_space_free(board, i):
-                                board[i] = playerLetter
+                                board[i//3][i%3] = playerLetter
                                 best = min(best, minimax(board, depth+1, not isMax, alpha, beta) + depth)
                                 beta = min(beta, best)
-                                board[i] = 0
+                                board[i//3][i%3] = 0
 
                                 if alpha >= beta:
                                         break
@@ -64,19 +64,21 @@ def minimax_select_move(board):
 
         for i in range(9):
                 if is_space_free(board, i):
-                        board[i] = computerLetter
+                        board[i//3][i%3] = computerLetter
 
                         moveVal = minimax(board, 0, False, -1000, 1000)
 
-                        board[i] = 0
+                        board[i//3][i%3] = 0
 
                         if moveVal > bestVal:
                                 bestMove = i
                                 bestVal = moveVal
 
-        return bestMove
+        moveProbs = [0 for _ in range(9)]
+        moveProbs[bestMove] = 1
+        return bestMove, moveProbs
 
 
 def is_board_full(board):
-         return 0 not in board
+         return 0 not in board[0] and 0 not in board[1] and 0 not in board[2]
 
